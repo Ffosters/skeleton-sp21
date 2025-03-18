5c42496cc43d5763fcfe925b0231e24d3c09e346
Ffosters <EM_Wallr@outlook.com>
2025-03-18 22:02:28 +0800

    DequeV1

---
+++ proj1/deque/ArrayDeque.java
@@ -0,0 +1,160 @@
+package deque;
+import java.util.Iterator;
+
+public class ArrayDeque<Rice> implements Iterable<Rice> {
+    private Rice[] aArray;
+    private int size;
+    public int capacity = 8;
+    private int front = 0;
+    private int rear = 0;
+    private double spaceAvaliable = 0.25;//0.25;
+/*      front 恒定向左移
+        rear 恒定向右移
+ */
+    public ArrayDeque(){
+        aArray = (Rice[]) new Object[capacity];                                                    //aArray = new Rice[capacity];
+        size = 0;
+    }
+
+
+
+    public void addFirst(Rice value){
+        if(size == capacity){resize(true);}
+        front = (front - 1 + capacity) % capacity;  //左移
+        aArray[front] = value;                      //存入数值
+        size++;
+    }
+    public void addLast(Rice value){
+        if(size == capacity){resize(true);}
+        aArray[rear] = value;                   //存入数值
+        rear = (rear + 1) % capacity;           //右移
+        size++;
+    }
+
+    public void removeFirst(){
+        if(size == 0){return;}
+        aArray[front] = null;
+        front = (front + 1) % capacity;
+        size--;
+        checkSize();
+    }
+
+    public void removeLast(){
+        if(size == 0){return;}
+        aArray[(rear - 1 + capacity) % capacity] = null;
+        rear = (rear - 1 + capacity) % capacity;
+        size--;
+        checkSize();
+    }
+
+    public Rice get(int index){
+        if(aArray[index] == null){return null;}
+        return aArray[index];
+    }
+
+
+
+    /*
+        这里应该用arraycopy更好一些 但是没有用 不改了 LLD也是一样的
+     */
+
+    public ArrayDeque(ArrayDeque other){//aArray = new Rice[capacity];
+        size = 0;
+        aArray = (Rice[]) new Object[other.capacity];
+        this.capacity = other.capacity;
+        for(int i = other.front; i != other.rear ; i = (i + 1) % capacity){
+            this.addLast((Rice)other.aArray[i]);
+        }
+
+    }
+
+    /*      --关于resize的修改问题
+           最好不要修改类，直接修改 Rice 数组比较好
+     */
+    public void resize(boolean flag){
+       // ArrayDeque newArray = new ArrayDeque();
+        Rice[] newArray = (Rice[]) new Object[resetCapacity(flag)];
+                                                                    //newArray只是rice类型的数组而这个数组并没有addlast方法；
+        for(int i = 0; i < size(); i++){                                //newArray.resetCapacity();
+            newArray[i] = aArray[front];
+            front =  (front + 1) % (capacity / 2);
+        }
+        aArray = newArray;
+        front = 0;
+        rear = size();
+    }
+
+    public int resetCapacity(boolean flag){
+        if(flag) {return capacity *= 2;}
+        else return capacity/=2;
+    }
+
+    public boolean isEmpty(){
+        return front == rear;
+    }
+
+    //Iterator
+    private class inIterator implements Iterator<Rice>{
+        public int iFront;
+        public int iRear;
+        public int pos;
+        public inIterator(){
+            iFront = front;
+            iRear = rear;
+            pos = front;
+        }
+
+        @Override
+        public boolean hasNext(){
+            return pos == iRear;
+        }
+
+        @Override
+        public Rice next(){
+            Rice tData = (Rice)aArray[pos];
+            pos = (pos + 1) % capacity;
+            return tData;
+        }
+    }
+
+    public Iterator<Rice> iterator(){
+        return new inIterator();
+    }
+
+    @Override
+    public boolean equals(Object o){
+        if(o instanceof ArrayDeque){
+            ArrayDeque other = (ArrayDeque) o;
+            if(other.size != size){return false;}
+            Iterator<Rice> origIter = this.iterator();
+            Iterator<Rice> compIter = other.iterator();
+            while(origIter.hasNext() && compIter.hasNext()){
+                if(origIter.next() != compIter.next()){return false;}  //不太清楚到底应该用 != 还是 equals
+            }
+        }
+        return true;
+    }
+
+
+
+
+
+    public void printDeque(){
+        for(int i = front; i != rear; i = (i + 1) % capacity){
+            System.out.print(aArray[i]+ " ");
+        }
+        System.out.println();
+    }
+
+    private void checkSize(){
+        if(capacity < 16 || size() / capacity > spaceAvaliable){return;}
+        if(size() < spaceAvaliable * capacity){
+            //resize(capacity / size());
+            resize(false);
+        }
+
+    }
+
+    public int size(){return size;}
+
+}
---
+++ proj1/deque/LinkedListDeque.java
@@ -0,0 +1,162 @@
+package deque;
+import java.util.Iterator;
+public class LinkedListDeque<Apple> implements Iterable<Apple> {
+    private IntNode sentinelF;
+    private IntNode sentinelL;
+    private int size;
+        /*  关于sentinel的处理
+    有两种方式   1.本类所写的 两个哨兵一头一尾
+                2.形成环    头尾相连 只要一个 sentinel sentinel指向头
+         */
+    public class IntNode {
+        public IntNode prev;
+        public IntNode next;
+        public Apple data;
+
+        public IntNode(Apple d,IntNode p,IntNode n){
+            data = d;
+            prev = p;
+            next = n;
+        }
+
+    }
+
+    public LinkedListDeque(){
+        size = 0;
+        sentinelF = new IntNode(null,null,null);              //sentinelF = new IntNode(null,null,sentinelL); //因为sentinelL还未创建所以这样是无效的
+        sentinelL = new IntNode(null,null,null);
+        sentinelF.next = sentinelL;
+        sentinelL.prev = sentinelF;//sentinelL = new IntNode(null,sentinelF,null);
+    }
+
+    public LinkedListDeque(LinkedListDeque other){
+        //LinkedListDeque newLLD = new LinkedListDeque();
+        size = 0;
+        sentinelF = new IntNode(null,null,null);
+        sentinelL = new IntNode(null,null,null);
+        sentinelF.next = sentinelL;
+        sentinelL.prev = sentinelF;
+        IntNode s = other.sentinelF.next;
+        while(s != other.sentinelL ){
+            this.addLast(s.data);
+            s = s.next;
+        }
+
+    }
+
+    //添加
+    public void addFirst(Apple data){
+        if(data == null){throw new IllegalArgumentException("can't add null");}
+        IntNode t = sentinelF.next;
+        sentinelF.next = new IntNode(data,sentinelF,sentinelF.next);
+        t.prev = sentinelF.next;
+        size++;
+    }
+
+    public void addLast(Apple data){
+        if(data == null){throw new IllegalArgumentException("can't add null");}
+        IntNode t = sentinelL.prev;
+        sentinelL.prev = new IntNode(data,sentinelL.prev,sentinelL);
+        t.next = sentinelL.prev;
+        size++;
+    }
+
+    //删除
+    public void removeLast(){
+        //是否要查空？
+        //if(sentinelL.prev == null){return;}
+        if(isEmpty()){return;}
+        IntNode t = sentinelL.prev.prev;
+        t.next = sentinelL;
+        sentinelL.prev = t;    //空指针异常？   不会 如果不是空，只会指向头 / 尾
+        size--;
+    }
+    public void removeFirst(){
+        //if(sentinelF.next == null){return;}  //有问题 错误的 修正为下面的
+        if(isEmpty()){return;}
+        IntNode t = sentinelF.next.next;
+        t.prev = sentinelF;
+        sentinelF.next = t;
+        size--;
+    }
+
+    public int size(){
+        return size;
+    }
+
+    public boolean isEmpty(){
+        return sentinelF.next == sentinelL;
+    }
+
+    private class inIterator implements Iterator<Apple>{
+        IntNode curr;
+        public inIterator(){
+                curr = sentinelF;
+        }
+
+        @Override
+        public boolean hasNext(){
+            return curr!= sentinelL;
+        }
+
+        @Override
+        public Apple next(){
+            Apple tApple = (Apple)curr.data;
+            curr = curr.next;
+            return tApple;
+        }
+    }
+
+    public Iterator<Apple> iterator(){
+        return new inIterator();
+    }
+
+    @Override
+    public boolean equals(Object o){
+        if(o == this){return true;} //如果是想比较两个不一样的 这时候应该是false吧
+        if(o instanceof LinkedListDeque){  //if(o instanceof LinkedListDeque oLLD) 这个版本不能使用
+            LinkedListDeque oLLD = (LinkedListDeque) o;
+            if(oLLD.size != this.size){return false;}
+            Iterator<Apple> origItr = this.iterator();
+            Iterator<Apple> compItr = oLLD.iterator();
+            while(origItr.hasNext() && compItr.hasNext()){
+                if(!origItr.next().equals(compItr.next())){return false;}
+            }
+        }
+        return true;
+    }
+
+
+
+
+
+    public Apple get(int index){
+        if(isEmpty()){return null;}
+        IntNode t = sentinelF.next;
+        if(index > size){return null;}
+        while(index != 0){
+            t = t.next;
+            index--;
+        }
+        return t.data;
+    }
+
+    public void printDeque(){
+        IntNode t = sentinelF.next;
+        while(t != sentinelL){
+            System.out.print(t.data + " ");
+            t = t.next;
+        }
+        System.out.println();
+    }
+
+    public Apple getRecursive(int index,IntNode curr) {
+        if(curr == null){return null;}
+        if(index == 0){return curr.data;}
+        return getRecursive(index--,curr.next);
+    }
+
+
+
+
+}
